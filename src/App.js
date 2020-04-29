597cc1012ddd933e3585efb440e93fff9ed3bb31
mariamisa <maryaam1998@hotmail.com>
2020-04-29 05:01:00 +0300

    setup react & file structure relates #2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Films from './componant/Films'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Films/>
+    </div>
+  );
+}
+
+export default App;
